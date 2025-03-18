cf7e91ecb866897168bf90b0ebfe26734a1f6226
adamafriansyahb <adamafriansyahb@gmail.com>
2025-03-18 14:46:09 +0700

    initial commit

---
+++ apps/web/container/home/Home.container.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@repo/ui/button';
+
+const Home = () => {
+  return (
+    <>
+      <Button appName="tes">Click Me</Button>
+      <h1>Welcome to my app</h1>
+    </>
+  );
+};
+
+export default Home;
